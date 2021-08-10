d809b2bacee582bb183d40257cae20248bbcbd9d
Nick Aldridge <naldridge1360@gmail.com>
2021-08-10 09:20:34 -0500

    final commit before presentation

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -8,12 +8,12 @@ export default function WeatherData({ weather, convertTime, convertDay }) {
     return (
         <div className="current_temp">
             <h2>{convertDay(weather.dt)}</h2>
-            <p>Current Temp:{Math.round(weather.temp.day)}°F</p>
-            <p>Feels Like:{Math.round(weather.feels_like.day)}°F</p>
-            <p>Humidity:{weather.humidity}%</p>
-            <p>Sunrise:{convertTime(weather.sunrise)}</p>
-            <p>Sunset:{convertTime(weather.sunset)}</p>
-            <p>Weather:{weather?.weather[0]?.description}</p>
+            <p>Current Temp: {Math.round(weather.temp.day)}°F</p>
+            <p>Feels Like: {Math.round(weather.feels_like.day)}°F</p>
+            <p>Humidity: {weather.humidity}%</p>
+            <p>Sunrise: {convertTime(weather.sunrise)}</p>
+            <p>Sunset: {convertTime(weather.sunset)}</p>
+            <p>Weather: {weather?.weather[0]?.description}</p>
             <i className={`owf owf-${weather?.weather[0].id} owf-5x`}></i>
         </div>
     )
